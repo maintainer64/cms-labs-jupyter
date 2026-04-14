eddbdfe1d801058e51406cd56af786d5f5d5d083
Danil Gubanov <3066897-gabenD@users.noreply.gitlab.com>
2026-04-14 18:41:29 +0500

    fix: profile_list args

--- cmsspawner/spawner/spawner.py
+++ cmsspawner/spawner/spawner.py
@@ -18,7 +18,7 @@ class CMSSpawner(KubeSpawner):
         self.rpc = CMSRpcClient()
         return await super()._start()
 
-    async def profile_list(self):
+    async def profile_list(self, *args, **kwargs) -> list | None:
         if not self.user:
             return None
         auth_state = await self.user.get_auth_state()
