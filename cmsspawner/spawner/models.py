8fea3783b9c1a9da4d83d15f655534f4c5fe5279
Danil Gubanov <3066897-gabenD@users.noreply.gitlab.com>
2026-04-15 15:10:28 +0500

    fix: models

--- cmsspawner/spawner/models.py
+++ cmsspawner/spawner/models.py
@@ -4,6 +4,6 @@ from dataclasses import dataclass
 @dataclass
 class UserInfo:
     user_id: str
-    username: str = auth_state["oauth_user"]["username"]
-    email: str = auth_state["oauth_user"]["email"]
-    name: str = auth_state["oauth_user"]["name"]
+    username: str
+    email: str
+    name: str
