854cbedaf43f31ca908297c5d0bebb4a4a9cc308
Danil Gubanov <3066897-gabenD@users.noreply.gitlab.com>
2026-04-17 18:52:00 +0500

    fix: KubeSpawner

--- cmsspawner/spawner/route.py
+++ cmsspawner/spawner/route.py
@@ -8,6 +8,7 @@ from tornado import web
 from tornado.web import HTTPError
 
 from cmsspawner.spawner.models import UserInfo
+from cmsspawner.spawner.rpc import CMSRpcClient
 
 
 @dataclass
@@ -49,15 +50,45 @@ class SecondStepHandler(BaseHandler):
                 400,
                 "Параметры лабораторной работы неверные.\nПожалуйста, вернитесь в Moodle и попробуйте запустить лабораторную работу снова."
             )
-        redirect_url = self.get_query_git_params(extra=extra)
-        raise HTTPError(
-            410,
-            "Пока успех"
+        rpc_client = CMSRpcClient()
+        attempts = await rpc_client.list_attempts(
+            attempt_ids=[extra.attempt_id],
+            user_ids=[user.user_id],
+            limit=5000,
+            offset=0,
         )
+        if not attempts:
+            raise HTTPError(
+                400,
+                "Отсутсвует номер попытки.\nПожалуйста, вернитесь в Moodle и попробуйте запустить лабораторную работу снова."
+            )
+        attempt = attempts[0]
+        attempt_id = attempt['attempt_id']
+        attempt_name = attempt.get('lti_routing_name') or attempt.get("user_name") or f"Attempt {attempt_id}"
+        if attempt_id != extra.attempt_id:
+            raise HTTPError(
+                400,
+                "Номер попытки не совпадает в запросе.\nПожалуйста, вернитесь в Moodle и попробуйте запустить лабораторную работу снова."
+            )
+        if attempt_id not in user.spawners:
+            self.log.info(f"Spawning server '{attempt_id}' for user {profile.email}")
+            try:
+                await user.spawn(
+                    server_name=attempt_id,
+                    user_options={"profile": attempt_id, "name": attempt_name}
+                )
+            except Exception as e:
+                self.log.error(f"Failed to spawn server '{attempt_id}' for {profile.email}: {e}")
+                raise HTTPError(
+                    500,
+                    f"{e}\nПожалуйста, вернитесь в Moodle и попробуйте запустить лабораторную работу снова."
+                )
+        redirect_url = self.get_redirect_complete_params(extra=extra, profile=profile)
+        self.redirect(redirect_url)
 
     async def get_user_profile(self) -> UserInfo | None:
         user = await self.get_current_user()
-        if user:
+        if not user:
             self.log.info("Current user is empty")
             return None
         auth_state = await user.get_auth_state()
@@ -88,7 +119,7 @@ class SecondStepHandler(BaseHandler):
             self.log.error(f"Failed to decode extra param: {e}")
             return None
 
-    def get_query_git_params(self, extra: SSOTokenPublicExtraParams) -> str | None:
+    def get_redirect_complete_params(self, extra: SSOTokenPublicExtraParams, profile: UserInfo) -> str:
         path = extra.pnet_labs_path.strip("/")
         base_path = path.split("/")[0]
         params = {
@@ -97,4 +128,4 @@ class SecondStepHandler(BaseHandler):
             "branch": self.git_branch
         }
         query_string = urlencode(params)
-        return f"/hub/user-redirect/{extra.attempt_id}/git-pull?{query_string}"
+        return f"/user/{profile.username}/{extra.attempt_id}/git-pull?{query_string}"
--- cmsspawner/spawner/spawner.py
+++ cmsspawner/spawner/spawner.py
@@ -2,8 +2,6 @@ import logging
 
 from tornado import web
 
-from .models import UserInfo
-from .rpc import CMSRpcClient
 from kubespawner import KubeSpawner
 from .utils import setup_logger
 
@@ -14,12 +12,6 @@ class CMSSpawner(KubeSpawner):
         super().__init__(*args, **kwargs)
         self.logger = setup_logger(__name__, logging.INFO)
         self.logger.info('Start working with CMSSpawner')
-        self._rpc: CMSRpcClient | None = None
-
-    async def get_rpc_client(self) -> CMSRpcClient:
-        if self._rpc is None:
-            self._rpc = CMSRpcClient()
-        return self._rpc
 
     async def _start(self):
         if not self.name:
@@ -38,67 +30,3 @@ class CMSSpawner(KubeSpawner):
             self.env['ATTEMPT_ID'] = attempt_id
             self.log.info(f"Updated extra_labels with attempt_id: {attempt_id}")
         return await super()._start()
-
-    async def get_options_form(self):
-        profiles = await self.profile_list(self)
-        try:
-            user_name = self.user.name
-        except Exception:
-            user_name = None
-        if not profiles:
-            self.log.warning(f"User {user_name} tried to login but has no active attempts.")
-            raise web.HTTPError(
-                403,
-                "У вас нет активных сессий в Moodle для запуска сервера."
-            )
-        return await super().get_options_form()
-
-    async def get_user_profile(self, current_spawner: KubeSpawner) -> UserInfo | None:
-        if not current_spawner.user:
-            self.log.info("Current user is empty")
-            return None
-        auth_state = await current_spawner.user.get_auth_state()
-        if not auth_state:
-            self.log.info("Current user state is empty")
-            return None
-        return UserInfo(
-            user_id=auth_state["oauth_user"]["sub"],
-            username=auth_state["oauth_user"]["username"],
-            email=auth_state["oauth_user"]["email"],
-            name=auth_state["oauth_user"]["name"],
-        )
-
-    async def profile_list(self, current_spawner: KubeSpawner) -> list | None:
-        if not current_spawner.name:
-            self.log.warning(f"Attempted to fetch profiles without a specified servername.")
-            raise web.HTTPError(
-                400,
-                "Запуск стандартного сервера запрещен. "
-                "Пожалуйста, используйте ссылки из Moodle для запуска лабораторных работ."
-            )
-        self.log.info("Fetching profiles for user")
-        user = await self.get_user_profile(current_spawner)
-        if not user:
-            return None
-        rpc = await self.get_rpc_client()
-        attempts = await rpc.list_attempts(
-            attempt_ids=[str(current_spawner.name)],
-            user_ids=[user.user_id],
-            limit=5000,
-            offset=0,
-        )
-        self.log.info(f"Profile list count {len(attempts)} by user {user.username}")
-        profiles = []
-        for attempt in attempts:
-            attempt_id = attempt['attempt_id']
-            attempt_number = attempt['id']
-            display_name = attempt.get('lti_routing_name') or attempt.get("user_name") or f"Attempt {attempt_id}"
-            description = f"Элемент курса совершён с номером попытки attempt_id({attempt_number})"
-            profile = {
-                'slug': attempt_id,
-                'display_name': display_name,
-                'default': False,
-                'description': description,
-            }
-            profiles.append(profile)
-        return profiles
